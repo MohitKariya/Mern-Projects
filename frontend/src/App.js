6e4315eb543ce9509b9025843bc2444fd5f10d1a
pragneshborad <pragneshborad@users.noreply.github.com>
2024-01-08 22:02:02 +0530

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from "./component/layout/Header.js"
-import {BrowserRouter as Router} from 'react-router-dom' 
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom' 
 import WebFont from"webfontloader"
 import React from 'react';
+import Footer from "./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
+
 
 function App() {
   React.useEffect(() => {
@@ -12,10 +14,15 @@ function App() {
         families:["Roboto","Droid Sans","Chilanka"],
       },
     });
+    
   },[]);
   return (
   <Router>
     <Header />
+    <Routes>
+    <Route path="/" exact element={<Home />} />
+    </Routes>
+    <Footer />
   </Router>
   );
 }
